8841d0a4331efc500a1720a9f7e7442ea5023eb2
WEB-AID <web.aid.dev@gmail.com>
2025-01-29 14:08:58 +0100

    м4с334амс4м3а34ам

--- src/shared/lib/providers/QueryProvider.tsx
+++ src/shared/lib/providers/QueryProvider.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+// import { useState } from "react";
 
 export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient()); // Обертка useState для мемоизации
+  // const [queryClient] = useState(() => new QueryClient());
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
