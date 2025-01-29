7b779a25d59cb15fabfc0e8e7a11dcbf016c246b
WEB-AID <web.aid.dev@gmail.com>
2025-01-29 23:08:07 +0100

    ec22ec2ec2c2ec

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://alltagshelfer-nest-production.up.railway.app/",
   withCredentials: true, // Для передачи refresh-token в cookies
 });
 
