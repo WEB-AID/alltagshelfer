239cd5568b1bdc82134b72f6d4cf4e7f253a3505
WEB-AID <web.aid.dev@gmail.com>
2025-02-02 12:00:19 +0100

    ZustandStateImplemented2

--- src/features/auth/model/useLogout.ts
+++ src/features/auth/model/useLogout.ts
@@ -1,12 +1,20 @@
 import { useAuthStore } from "@/entities/Auth/model/authStore";
 import { useUserStore } from "@/entities/User/model/userStore";
+import { axiosInstance } from "@/shared/api/axios";
 
 export const useLogout = () => {
   const logout = useAuthStore((state) => state.logout);
   const clearUser = useUserStore((state) => state.clearUser);
 
-  return () => {
-    logout();
-    clearUser();
+  return async () => {
+    try {
+      await axiosInstance.get("/auth/logout"); // Логаут на сервере
+    } catch (error) {
+      console.error("Ошибка при логауте:", error);
+    } finally {
+      localStorage.removeItem("access_token"); // Удаляем accessToken из localStorage
+      logout(); // Обновляем Zustand: пользователь не авторизован
+      clearUser(); // Очищаем данные пользователя
+    }
   };
 };
