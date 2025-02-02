7042c95b0ef7e779c41895baf5578ff81e3b2607
WEB-AID <web.aid.dev@gmail.com>
2025-02-02 09:47:24 +0100

    GoogleTokenSuccessCatchFinal

--- src/views/success-google-page/ui/SuccessGoogle.tsx
+++ src/views/success-google-page/ui/SuccessGoogle.tsx
@@ -15,7 +15,7 @@ export default function AuthSuccess() {
         try {
           const accessToken = await verifyGoogleToken(token);
           localStorage.setItem("accessToken", accessToken);
-          console.error("Все ок перенаправляем на главную токен:", token);
+          console.log("Все ок перенаправляем на главную токен:", token);
           router.push("/");
         } catch (error) {
           console.error("Ошибка при проверке токена:", error);
