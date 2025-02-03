bc7ec6d2c36573d58330d989cf0d4a85ef226752
WEB-AID <web.aid.dev@gmail.com>
2025-02-03 21:06:30 +0100

    AuthDialog8

--- src/shared/api/googleAuth.ts
+++ src/shared/api/googleAuth.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const verifyGoogleToken = async (token: string) => {
   const response = await axios.get("/api/auth/success-google", {
     params: { token },
-    withCredentials: true, // Для передачи HttpOnly куки
+    // withCredentials: true, // Для передачи HttpOnly куки
   });
   return response.data.accessToken;
 };
